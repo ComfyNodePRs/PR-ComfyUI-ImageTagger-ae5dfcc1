3ba538ac4539a73eb81704b993c76732a1ff6b7c
purpen <purpen.w@gmail.com>
2024-11-28 01:20:45 +0800

    add init code

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .image_captioning import ImageCaptioning
+
+NODE_CLASS_MAPPINGS = {
+    "AIRedoonImageCaptioning": ImageCaptioning
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AIRedoonImageCaptioning": "AIRedoon Image Caption"
+}
